322bf1445e7be9445a4916a7991707f2355fa980
Monk Liu <Monk.Liu@amd.com>
2018-12-11 15:30:07 +0800

    automatically use multi thread for building
    Signed-off-by: Monk Liu <Monk.Liu@amd.com>

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-fakeroot make-kpkg --initrd --append-to-version=$1 kernel_image kernel_headers -j
+fakeroot make-kpkg --initrd --append-to-version=$1 kernel_image kernel_headers -j `grep -c ^processor /proc/cpuinfo`
